a5aca2c999168a96c865f802e2ff5957515a8180
Mila Volia <milavolia@MacBook-Air-Mila.local>
2023-09-01 11:18:33 +0300

    promo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Header/Header";
+import Promo from "./components/Promo/Promo";
 
 function App() {
   return (
-    <Header />
+    <div className="App">
+      <Header />
+      <Promo />
+    </div>
   );
 }
 
